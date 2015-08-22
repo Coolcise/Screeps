0f56438f8639bb595372b70fbf5aa983c9a610c7
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-22 15:45:35 +0200

    Added NOT FINISHED

---
+++ dist/carrier.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    behavior: function(creep){
+        if (creep.carry.energy == 0){
+            creep.moveTo(Game.spawns.Spawn1);
+            Game.spawns.Spawn1.transferEnergy(creep);
+
+        }
+        else{
+            var target = Game.rooms.W9S11.controller;
+            if (target){
+                creep.moveTo(target);
+                creep.upgradeController(target);
+            }
+        }
+    }
+}
