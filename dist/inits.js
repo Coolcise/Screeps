f0dd6d4b820646438a5865fa3eb1f92100247c58
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-22 15:48:24 +0200

    Added more arrays

--- dist/inits.js
+++ dist/inits.js
@@ -1,9 +1,11 @@
 module.exports = {
 
   initRoom: function(roomName){
-    var currentRoom = roomName;
-    currentRoom.harvesterArray = [];
-    currentRoom.builderArray = [];
-    currentRoom.guardArray = [];
+    Memory.rooms.roomName.harvesterArray = new Array();
+    Memory.rooms.roomName.builderArray = new Array();
+    Memory.rooms.roomName.upgraderArray = new Array();
+    Memory.rooms.roomName.carrierArray = new Array();
+    Memory.rooms.roomName.guardArray = new Array();
+    Memory.rooms.roomName.energyStorageArray = new Array();
   }
 }
