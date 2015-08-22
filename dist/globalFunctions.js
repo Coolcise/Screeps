0c6e6b63ad3b9d096592c6a74936c0e5474bf745
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-22 15:48:15 +0200

    Added newConstructionSite

--- dist/globalFunctions.js
+++ dist/globalFunctions.js
@@ -7,24 +7,53 @@ module.exports = {
       location = Game.spawns.Spawn1;
     }
 
-    var creepInfo = partsLib.getCreepParts(type);
-    location.createCreep(creepInfo[1], null, {role: creepInfo[0]});
+    var parts = partsLib.getCreepParts(type);
+
+    if (Game.spawns.Spawn1.energy >= partslib.getCost(parts)){
+      location.createCreep(parts, null, {role: type});
+    }
 
   },
 
-  assignJob: function(creepID, pos) {
+  newConstructionSite: function(structure, toPosX, toPosY, fromPosX, fromPosY, room){
+    if (!room){
+      var room = Game.rooms.W9S11;
+    }
+
+    var path = false;
+
+    var toPos = new RoomPosition(toPosX, toPosY, room);
+
+    if (fromPosX && fromPosY) {
+      var fromPos = new RoomPosition(fromPosX, fromPosY, room);
+      path = room.findPath(fromPos, toPos);
+      console.log(path);
+    }
+
+    if(path) {
+      for(i = 0; i < path.length; i++){
+        room.createConstructionSite(path[i], structure);
+      }
+    }
+    else{
+      room.createConstructionSite(toPos, structure);
+    }
 
   },
 
-  stopJob: function(creepID) {
+  assignJob: function(creepName, x, y) {
 
   },
 
-  reassignRole: function(creepID, newRole) {
+  stopJob: function(creepName) {
 
   },
 
-  killCreep: function(creepID) {
+  reassignRole: function(creepName, newRole) {
+
+  },
+
+  killCreep: function(creepName) {
 
   }
 
