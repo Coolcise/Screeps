c006001d2b8e1d0c6c5ec482a82dacfbbd28b52e
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-22 16:35:55 +0200

    Added maintainCreepCount

--- dist/maintenance.js
+++ dist/maintenance.js
@@ -1,6 +1,36 @@
+var globalFunctions = require('globalFunctions');
+
 module.exports = {
 
     maintainCreepCount: function() {
-      
+      var creeps = Game.creeps;
+
+      var harvesterCount = 0;
+      var builderCount = 0;
+      var upgraderCount = 0;
+
+      for (i = 0; i < creeps.length; i++) {
+        var role = creeps[i].memory.role;
+        switch(role){
+          case 'harvester':
+            harvesterCount++;
+            break;
+          case 'builder':
+            builderCount++;
+            break;
+          case 'upgrader':
+            upgraderCount++;
+            break;
+        }
+      }
+
+      if (harvesterCount < Memory.rooms.W9S11.harvesters) {
+        globalFunctions.spawnCreep('harvester');
+      }
+      else if (builderCount < Memory.rooms.W9S11 .builders){
+        globalFunctions.spawnCreep('builder');
+      }
+      else if (upgraderCount < Memory.rooms.W9S11.upgraders)
+        globalFunctions.spawnCreep('upgrader');
     }
 }
