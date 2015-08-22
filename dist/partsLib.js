20a8ad92f598e93f4646440e1b42f0b5ad10e898
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-22 12:55:20 +0200

    Added upgrader

--- dist/partsLib.js
+++ dist/partsLib.js
@@ -26,7 +26,8 @@ module.exports = {
 
 		var parts = new Array();
    	parts[harvester] = ['harvester', [WORK, MOVE, CARRY]];
-		parts[builder] = ['builder', [WORK, WORK, CARRY, MOVE]
+		parts[builder] = ['builder', [WORK, WORK, CARRY, MOVE];
+		parts[upgrader] = ['upgrader', [WORK, WORK, CARRY, MOVE]
 		parts[guard] = ['guard', [TOUGH, MOVE, MOVE, ATTACK]];
 		parts[pathfinder] = ['pathfinder', [MOVE, MOVE, MOVE, MOVE]];
 
