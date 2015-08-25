4df0971fd433ddb168558c5d3f70393c0793ac6b
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-25 15:53:15 +0200

    Revert "Revert "Added automatic array updates""
    This reverts commit c05df3741bf0845149d46ecc6e07a229186d5356.

--- dist/main.js
+++ dist/main.js
@@ -2,29 +2,33 @@ var harvester = require('harvester');
 var builder = require('builder');
 var upgrader = require('upgrader');
 var guard = require('guard');
-var inits = require('inits');
 var maintenance = require('maintenance');
 
 maintenance.maintainCreepCount();
 
+if (!(Game.time % 50)){
+  for (var roomName in Game.rooms) {
+    var roomName = Game.rooms[roomName];
+    maintenance.updateArrays(roomName)
+  }
+}
+
 for (var name in Game.creeps){
-    var creep = Game.creeps[name];
+  var creep = Game.creeps[name];
 
-    if (creep.memory.role == 'harvester'){
-        harvester.behavior(creep);
-    }
+  if (creep.memory.role == 'harvester'){
+    harvester.behavior(creep);
+  }
 
-    if (creep.memory.role == 'builder'){
+  if (creep.memory.role == 'builder'){
+    builder.behavior(creep);
+  }
 
-        builder.behavior(creep);
-    }
+  if (creep.memory.role == 'upgrader'){
+    upgrader.behavior(creep);
+  }
 
-    if (creep.memory.role == 'upgrader'){
-
-        upgrader.behavior(creep);
-    }
-
-    if (creep.memory.role == 'guard'){
-        guard.behavior(creep);
-    }
+  if (creep.memory.role == 'guard'){
+    guard.behavior(creep);
+  }
 }
