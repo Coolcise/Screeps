58a6e4d722f65180ff7cb50f0c191f44f32d2f44
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-25 15:52:57 +0200

    Revert "Revert "Indentation""
    This reverts commit 893e39b4673616dde065007fdb0178788fec589d.

--- dist/upgrader.js
+++ dist/upgrader.js
@@ -1,18 +1,18 @@
 module.exports = {
 
-    behavior: function(creep){
-        if (creep.carry.energy == 0){
-            creep.moveTo(Game.spawns.Spawn1);
-            if (Game.spawns.Spawn1.energy >= creep.carryCapacity){
-              Game.spawns.Spawn1.transferEnergy(creep);
-            }
-        }
-        else{
-            var target = Game.rooms.W9S11.controller;
-            if (target){
-                creep.moveTo(target);
-                creep.upgradeController(target);
-            }
-        }
+  behavior: function(creep){
+    if (creep.carry.energy == 0){
+      creep.moveTo(Game.spawns.Spawn1);
+      if (Game.spawns.Spawn1.energy >= creep.carryCapacity){
+        Game.spawns.Spawn1.transferEnergy(creep);
+      }
     }
+    else{
+      var target = creep.room.controller;
+      if (target){
+        creep.moveTo(target);
+        creep.upgradeController(target);
+      }
+    }
+  }
 }
