9e58722f3ce7b935d03e7aa28d8375c0b5bad34d
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-25 15:50:17 +0200

    Revert "Added more tiers"
    This reverts commit 5b6e2a2649b3a1908984945260c4faf8dbb1b64b.

--- dist/partsLib.js
+++ dist/partsLib.js
@@ -12,61 +12,21 @@ module.exports = {
 		costDictionary[TOUGH] = 10;
 
 		for (var i = 0; i < parts.length; ++i)
-		totalCost += costDictionary[parts[i]];
+			totalCost += costDictionary[parts[i]];
 
 		return totalCost;
 	},
 
-	getCreepParts: function(creepType) {
+  getCreepParts: function(creepType) {
 
 		var parts = new Array();
+   	parts['harvester'] = [WORK, WORK, MOVE, CARRY];
+		parts['builder'] = [WORK, WORK, CARRY, MOVE];
+		parts['upgrader'] = [WORK, WORK, CARRY, MOVE];
+		parts['guard'] = [TOUGH, MOVE, MOVE, ATTACK];
+		parts['carrier'] = [MOVE, MOVE, CARRY, CARRY, CARRY, CARRY];
 
-
-		//Add more variants when higher room level is aquired and update for more rooms
-		if(Memory.rooms.W9S11.extensionsArray.length >= 10) {						//800 energy max
-			
-			parts['harvester']	=	[	WORK, WORK, WORK, WORK, WORK, WORK,
-															CARRY, CARRY, CARRY,
-															MOVE	];
-			parts['builder'] 		=	[	WORK, WORK, WORK, WORK,
-															CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,
-															MOVE, MOVE	];
-			parts['upgrader'] 	= [	WORK, WORK, WORK, WORK, WORK, WORK, WORK,
-															CARRY, MOVE	];
-			parts['carrier']		=	[	CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,
-															CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,
-															MOVE, MOVE, MOVE	];
-			parts['mule']				= [	CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,
-															CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,
-															CARRY, MOVE	];
-			parts['guard'] 			=	[	ATTACK, TOUGH, MOVE, MOVE];			//TBD
-
-		}
-		else if(Memory.rooms.W9S11.extensionsArray.length >= 5) {				//550 energy max
-
-			parts['harvester'] 	= [	WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE	];
-			parts['builder'] 		= [	WORK, WORK, WORK, CARRY, CARRY, CARRY,
-															MOVE, MOVE	];
-			parts['upgrader']		= [	WORK, WORK, WORK, WORK, CARRY, MOVE	];
-			parts['carrier'] 		= [	CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,
-															CARRY, CARRY, MOVE, MOVE	];
-			parts['mule']				= [	CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY,
-															CARRY, CARRY, CARRY, MOVE	];
-			parts['guard'] 			= [	ATTACK, TOUGH, MOVE, MOVE	];			//TBD
-
-		}
-		else{
-																													//300 energy max
-			parts['harvester'] 		= [	WORK, WORK,  CARRY, MOVE	];
-			parts['builder'] 			= [	WORK, WORK, CARRY, MOVE	];
-			parts['upgrader'] 		= [	WORK, WORK, CARRY, MOVE	];
-			parts['carrier'] 			= [	CARRY, CARRY, CARRY, CARRY, MOVE, MOVE	];
-			parts['mule']					= [	CARRY, CARRY, CARRY, CARRY, CARRY, MOVE	];
-			parts['guard'] 				= [	ATTACK, TOUGH, MOVE, MOVE	];
-
-		}
-
-		return parts[creepType];
+    return parts[creepType];
 
 	}
 }
