c05df3741bf0845149d46ecc6e07a229186d5356
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-25 15:50:25 +0200

    Revert "Added automatic array updates"
    This reverts commit 186266fd84eb5614a3e99e7c9ddc9ae47cb036df.

--- dist/main.js
+++ dist/main.js
@@ -2,33 +2,29 @@ var harvester = require('harvester');
 var builder = require('builder');
 var upgrader = require('upgrader');
 var guard = require('guard');
+var inits = require('inits');
 var maintenance = require('maintenance');
 
 maintenance.maintainCreepCount();
 
-if (!(Game.time % 50)){
-  for (var roomName in Game.rooms) {
-    var roomName = Game.rooms[roomName];
-    maintenance.updateArrays(roomName)
-  }
-}
-
 for (var name in Game.creeps){
-  var creep = Game.creeps[name];
+    var creep = Game.creeps[name];
 
-  if (creep.memory.role == 'harvester'){
-    harvester.behavior(creep);
-  }
+    if (creep.memory.role == 'harvester'){
+        harvester.behavior(creep);
+    }
 
-  if (creep.memory.role == 'builder'){
-    builder.behavior(creep);
-  }
+    if (creep.memory.role == 'builder'){
 
-  if (creep.memory.role == 'upgrader'){
-    upgrader.behavior(creep);
-  }
+        builder.behavior(creep);
+    }
 
-  if (creep.memory.role == 'guard'){
-    guard.behavior(creep);
-  }
+    if (creep.memory.role == 'upgrader'){
+
+        upgrader.behavior(creep);
+    }
+
+    if (creep.memory.role == 'guard'){
+        guard.behavior(creep);
+    }
 }
--- dist/maintenance.js
+++ dist/maintenance.js
@@ -1,55 +1,39 @@
 var globalFunctions = require('globalFunctions');
-var roomMemory = Memory.rooms.roomName;
-
 
 module.exports = {
 
-  maintainCreepCount: function(roomName) {
+  maintainCreepCount: function() {
 
-    var harvesterCount = 0;
-    var builderCount = 0;
-    var upgraderCount = 0;
+      var harvesterCount = 0;
+      var builderCount = 0;
+      var upgraderCount = 0;
 
-    for (var name in Game.creeps) {
-      var role = Game.creeps[name].memory.role;
-      switch(role){
-        case 'harvester':
-        harvesterCount++;
-        break;
-        case 'builder':
-        builderCount++;
-        break;
-        case 'upgrader':
-        upgraderCount++;
-        break;
+      for (var name in Game.creeps) {
+        var role = Game.creeps[name].memory.role;
+        switch(role){
+          case 'harvester':
+            harvesterCount++;
+            break;
+          case 'builder':
+            builderCount++;
+            break;
+          case 'upgrader':
+            upgraderCount++;
+            break;
+        }
       }
-    }
 
-    if (harvesterCount < roomMemory.harvesters) {
-      globalFunctions.spawnCreep('harvester');
-    }
-    else if (builderCount < roomMemory .builders){
-      globalFunctions.spawnCreep('builder');
-    }
-    else if (upgraderCount < roomMemory.upgraders)
-    globalFunctions.spawnCreep('upgrader');
-  },
+    //console.log(harvesterCount, builderCount, upgraderCount);
 
-  updateArrays: function(roomName) {
-
-    initArrays(roomName);
-
-    deadCreepMemory();
-
-    var allStructures = Game.rooms.roomName.find(FIND_MY_STRUCTURES);
-
-    for (var structure in allStructures) {
-      if ((structure.structureType === STUCTURE_EXTENSION)
-      && !(roomMemory.extensionsArray[structure])) {
-        roomMemory.extensionsArray.push(structure);
+      if (harvesterCount < Memory.rooms.W9S11.harvesters) {
+        globalFunctions.spawnCreep('harvester');
       }
-    }
-  },
+      else if (builderCount < Memory.rooms.W9S11 .builders){
+        globalFunctions.spawnCreep('builder');
+      }
+      else if (upgraderCount < Memory.rooms.W9S11.upgraders)
+        globalFunctions.spawnCreep('upgrader');
+    },
 
   deadCreepMemory: function() {
 
@@ -59,36 +43,4 @@ module.exports = {
       }
     }
   }
-
-  initArrays: function(roomName) {
-
-    if (!roomMemory.harvesterArray){
-      roomMemory.harvesterArray = new Array();
-    }
-
-    if (!roomMemory.builderArray) {
-      roomMemory.builderArray = new Array();
-    }
-
-    if(!roomMemory.upgraderArray){
-      roomMemory.upgraderArray = new Array();
-    }
-
-    if (!roomMemory.carrierArray){
-      roomMemory.carrierArray = new Array();
-    }
-
-    if(!roomMemory.guardArray){
-      roomMemory.guardArray = new Array();
-    }
-
-    if(!roomMemory.extensionsArray){
-      roomMemory.extensionsArray = new Array();
-    }
-
-    if(!roomMemory.spawnQueue){
-      roomMemory.spawnQueue = new Array();
-    }
-  }
-
 }
