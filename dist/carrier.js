48dd5bd0ce9a78b958abc2321793a1c7bc62eb78
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-25 19:29:27 +0200

    carrier fixes and todo updates

--- dist/carrier.js
+++ dist/carrier.js
@@ -3,7 +3,7 @@ module.exports = {
   behavior: function(creep){
 
     var currentRoom = creep.room;
-    if (creep.carry.energy == 0){
+    if (creep.carry.energy === 0 || creep.carry.energy === 1){
       creep.moveTo(Game.spawns.Spawn1);
       Game.spawns.Spawn1.transferEnergy(creep);
 
@@ -11,14 +11,29 @@ module.exports = {
     else{
       var targets = Memory.rooms.W9S11.extensionsArray;
 
-      for (var extension in targets) {
-        var energyAmount = targets[extension].energy;
-        if (energyAmount < targets[extension].energyCapacity){
-          creep.moveTo(targets[extension]);
-          creep.transferEnergy(target[extension]);
+    //console.log(Memory.rooms.W9S11.extensionsArray[0].energy)
+
+      //for (var extension in targets) {
+        //var energyAmount = targets[extension].energy;
+       // console.log(energyAmount);
+        //if ((energyAmount === undefined) || (energyAmount < targets[extension].energyCapacity)){
+         // console.log(targets[extension]);
+         var allStructures = Game.rooms.W9S11.find(FIND_MY_STRUCTURES);
+
+    for (var structure in allStructures) {
+
+      if ((allStructures[structure].structureType === STRUCTURE_EXTENSION)
+      && (allStructures[structure].energy < allStructures[structure].energyCapacity)) {
+        creep.moveTo(allStructures[structure]);
+          creep.transferEnergy(allStructures[structure]);
           break;
+      }
+    }
+          creep.moveTo(allStructures[structure]);
+          creep.transferEnergy(allStructures[structure]);
+          //break;
         }
       }
     }
-  }
-}
+  //}
+//}
