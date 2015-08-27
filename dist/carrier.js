58ca7a087ad93ddeeb848304fb9ca491bcd4f884
David Horntvedt <dbhorntvedt@gmail.com>
2015-08-27 23:37:24 +0200

    Added pick up energy

--- dist/carrier.js
+++ dist/carrier.js
@@ -5,37 +5,34 @@ module.exports = {
     var currentRoom = creep.room;
     if (creep.carry.energy === 0 || creep.carry.energy === 1){
       creep.moveTo(Game.spawns.Spawn1);
-    if ((Game.spawns.Spawn1.energy >= (creep.carryCapacity / .75)) && !(Memory.rooms.E12N6.spawnQueue.length)){
-      Game.spawns.Spawn1.transferEnergy(creep);
-    }
+
+      var target = creep.pos.findClosest(FIND_DROPPED_ENERGY);
+      if(target) {
+        creep.moveTo(target);
+        creep.pickup(target);
+      }
+      else if ((Game.spawns.Spawn1.energy >= (creep.carryCapacity / .75)) && !(Memory.rooms.E12N6.spawnQueue.length)){
+        Game.spawns.Spawn1.transferEnergy(creep);
+      }
 
     }
     else{
       var targets = Memory.rooms.E12N6.extensionsArray;
 
-    //console.log(Memory.rooms.E12N6.extensionsArray[0].energy)
+      var allStructures = Game.rooms.E12N6.find(FIND_MY_STRUCTURES);
 
-      //for (var extension in targets) {
-        //var energyAmount = targets[extension].energy;
-       // console.log(energyAmount);
-        //if ((energyAmount === undefined) || (energyAmount < targets[extension].energyCapacity)){
-         // console.log(targets[extension]);
-         var allStructures = Game.rooms.E12N6.find(FIND_MY_STRUCTURES);
+      for (var structure in allStructures) {
 
-    for (var structure in allStructures) {
-
-      if ((allStructures[structure].structureType === STRUCTURE_EXTENSION)
-      && (allStructures[structure].energy < allStructures[structure].energyCapacity)) {
-        creep.moveTo(allStructures[structure]);
-          creep.transferEnergy(allStructures[structure]);
-          break;
-      }
-    }
+        if ((allStructures[structure].structureType === STRUCTURE_EXTENSION)
+        && (allStructures[structure].energy < allStructures[structure].energyCapacity)) {
           creep.moveTo(allStructures[structure]);
           creep.transferEnergy(allStructures[structure]);
-          //break;
+          break;
         }
       }
+      creep.moveTo(allStructures[structure]);
+      creep.transferEnergy(allStructures[structure]);
+
     }
-  //}
-//}
+  }
+}
